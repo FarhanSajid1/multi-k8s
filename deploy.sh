d052ce0ec34739b0ea677a17e909aea72ac915be
Farhan Sajid <FarhanSajid7861@gmail.com>
2019-03-15 00:58:23 -0400

    retry

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-docker build -t farhansajid2/multi-client:latest -t farhansajid2/multi-client:$SHA -f ./client/Dockerfile ./client/
-docker build -t farhansajid2/multi-server:latest -t farhansajid2/multi-server:$SHA -f ./server/Dockerfile ./server/
-docker build -t farhansajid2/multi-worker:latest -t farhansajid2/multi-worker:$SHA -f ./worker/Dockerfile ./worker/
+docker build -t farhansajid2/multi-client:latest -t farhansajid2/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t farhansajid2/multi-server:latest -t farhansajid2/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t farhansajid2/multi-worker:latest -t farhansajid2/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push farhansajid2/multi-client:latest
 docker push farhansajid2/multi-client:$SHA
@@ -16,3 +16,5 @@ kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=farhansajid2/multi-server:$SHA
 kubectl set image deployments/worker-deployment worker=farhansajid2/multi-worker:$SHA
 kubectl set image deployments/client-deployment client=farhansajid2/multi-client:$SHA
+
+
